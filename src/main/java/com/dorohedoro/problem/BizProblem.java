90afb9104f4a8b024d806501ae54a0b4c8e2fc1f
liuziyee <liuziyee1994@163.com>
2022-11-08 17:22:31 +0800

    :beers:

--- src/main/java/com/dorohedoro/problem/BizProblem.java
+++ src/main/java/com/dorohedoro/problem/BizProblem.java
@@ -6,7 +6,7 @@ import org.zalando.problem.Status;
 import java.net.URI;
 
 public class BizProblem extends AbstractThrowableProblem {
-    private static final URI TYPE = URI.create("/internal_server_error");
+    private static final URI TYPE = URI.create("/server");
 
     public BizProblem(String detail) {
         super(TYPE, "服务器错误", Status.INTERNAL_SERVER_ERROR, detail);
