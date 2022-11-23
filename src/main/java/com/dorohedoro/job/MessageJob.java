e093fad2a1b938f5d07dea4f6a9bc38233cac299
liuziyee <liuziyee1994@163.com>
2022-11-23 23:56:02 +0800

    :beers:

--- src/main/java/com/dorohedoro/job/MessageJob.java
+++ src/main/java/com/dorohedoro/job/MessageJob.java
@@ -14,7 +14,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
@@ -26,7 +25,7 @@ public class MessageJob {
 
     @Async
     public void send(String topic, Message message) {
-        log.debug("创建推送消息,投递MQ消息,该MQ消息用于创建消息推送记录");
+        log.info("创建推送消息,投递MQ消息,该MQ消息用于创建消息推送记录");
         try {
             channel.queueDeclare(topic, true, false, false, null); // 声明队列,该队列会绑定到默认交换机
             String msgId = messageService.createMessage(message);
@@ -41,7 +40,7 @@ public class MessageJob {
 
     @Async
     public void receive(String topic) {
-        log.debug("拉取MQ消息,创建消息推送记录");
+        log.info("拉取MQ消息,创建消息推送记录");
         try {
             channel.queueDeclare(topic, true, false, false, null);
             while (true) {
@@ -55,8 +54,8 @@ public class MessageJob {
                     msgPushRecord.setIsRead(false);
                     msgPushRecord.setIsLast(true);
                     messageService.createMsgPushRecord(msgPushRecord);
-                    TimeUnit.SECONDS.sleep(10);
-                    channel.basicAck(response.getEnvelope().getDeliveryTag(), false); // 手动签收
+                    //TimeUnit.SECONDS.sleep(25);
+                    channel.basicAck(response.getEnvelope().getDeliveryTag(), false); // 手动确认
                     continue;
                 }
                 break;
--- src/test/java/com/dorohedoro/MsgTest.java
+++ src/test/java/com/dorohedoro/MsgTest.java
@@ -1,15 +1,19 @@
 package com.dorohedoro;
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.thread.ThreadUtil;
+import com.dorohedoro.job.MessageJob;
 import com.dorohedoro.mongo.entity.Message;
 import com.dorohedoro.mongo.entity.MessagePushRecord;
 import com.dorohedoro.service.IMessageService;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -18,9 +22,11 @@ public class MsgTest {
 
     @Autowired
     private IMessageService messageService;
-    
+    @Autowired
+    private MessageJob messageJob;
+
     @Test
-    public void generateMsgPushRecords() {
+    public void createMsgPushRecords() {
         IntStream.range(0, 100).forEach(no -> {
             Message message = new Message();
             message.setSenderId(0L);
@@ -36,5 +42,22 @@ public class MsgTest {
             msgPushRecord.setIsLast(true);
             messageService.createMsgPushRecord(msgPushRecord);
         });
+    }
+
+    @Test
+    @SneakyThrows
+    public void mockPolling() {
+        // 这里模拟小程序轮询刷新消息推送记录接口,第一次轮询,甲消费者拉取到一条消息,第二次轮询,乙消费者也拉取到了甲还没有确认的同一条消息
+        Message message = new Message();
+        message.setSenderId(0L);
+        message.setSenderName("通知");
+        message.setCreateTime(DateUtil.date());
+        message.setMsg("测试消息");
+        messageJob.send("1", message);
+        
+        ThreadUtil.execAsync(() -> messageJob.receive("1"));
+        TimeUnit.SECONDS.sleep(5);
+        ThreadUtil.execAsync(() -> messageJob.receive("1"));
+        TimeUnit.MINUTES.sleep(5);
     }
 }
